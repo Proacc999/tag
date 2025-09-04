098d7a2bef7608b8762da897f4a4cd69d4ccfcbd
Proaccc999 <Proacc@mynes.com>
2025-09-04 18:55:46 +0530

    combining both

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+// index.js
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express with import!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
